6d02468b2d34f9d81d43fd2de2dcd49b5d6c5b2d
bingojay <60956142+bingojay@users.noreply.github.com>
2020-09-10 16:31:22 -0700

    Update binary_to_decimal.rb
    tested

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,11 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  decimal_sum = 0
+  binary_array.each_with_index do |bit, index|
+    decimal = bit * 2**(binary_array.length - index - 1)
+    decimal_sum = decimal_sum + decimal
+  end
+  return decimal_sum
 end
+
